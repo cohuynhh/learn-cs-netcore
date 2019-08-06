907a288a86347e4094126d994eacc6c51478a255
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-06 15:21:06 +0700

    Update

---
+++ CSHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chào C# NET CORE!");
+        }
+    }
+}

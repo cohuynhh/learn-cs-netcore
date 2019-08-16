b5a3abf337ff1f06776a65a0c17de9c70c799612
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-17 00:31:43 +0700

    Update

---
+++ CS05-for-while/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CS05_for_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // for (int i = 8;i <=10;i++)
+            // {
+            //     Console.WriteLine("Số i = " + i);
+            // }
+
+            // int j = 10;
+            // for (;j <=20;) 
+            // {
+            //     Console.WriteLine("Số j = " + j);
+            //     j += 2;
+            // }
+            // // Số j = 10
+            // // Số j = 12
+            // // Số j = 14
+            // // Số j = 16
+            // // Số j = 18
+            // // Số j = 20
+
+
+
+            // for (; ;);
+
+            // int i = 8;
+            // while (i <= 10)
+            // {
+            //     Console.WriteLine("Số i = " + i);
+            //     i++;
+            // }
+            // // Số i = 8
+            // // Số i = 9
+            // // Số i = 10
+
+
+            // for (int i = 10; i <= 20; i ++ )
+            // {
+            //     if (i % 3 != 0)
+            //         continue;
+            //     Console.WriteLine("Số i = " + i);
+            // }
+            // // Số i = 12
+            // // Số i = 15
+            // // Số i = 18
+
+
+            // int i = 0; 
+            // while (true) 
+            // {
+            //     Console.Write(" " + ++i);
+            //     if (i == 10)
+            //         break; // Thoát lặp
+            // }
+            // // 1 2 3 4 5 6 7 8 9 10
+
+
+        }
+    }
+}

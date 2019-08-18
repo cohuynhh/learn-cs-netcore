188f1dfbae18a33707e4438ec40abbb803a4cca1
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-19 00:21:59 +0700

    Update

---
+++ CS09-Anonymous-lambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS09_Anonymous_lambda
+{
+    class TinhToan 
+    {
+
+
+        int Tong(int x, int y) => x + y;  
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}

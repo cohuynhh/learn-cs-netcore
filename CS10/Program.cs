d24aa6f6566b76574c4bd560aa15767359526ff9
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-22 13:51:26 +0700

    CS10

---
+++ CS10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CS10
+{
+    
+
+    class Program
+    {
+        class Dog
+        {
+            string[] A = new string[] {};
+            public Dog() {
+                Console.WriteLine("Constructor");
+            }
+            ~Dog() {
+                Console.WriteLine("Destructor");
+            }
+        }
+
+        class C {
+            public C() {
+                Dog d = new Dog();
+
+            }
+        }
+        static void abc()
+        {
+            var a = new Dog();
+        }
+        static void Main(string[] args)
+        {
+            new Dog(); 
+        }
+    }
+}

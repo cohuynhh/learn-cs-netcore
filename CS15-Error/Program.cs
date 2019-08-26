a93fa3e71d611606b495f03e9480f3d85c750aca
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-26 20:54:38 +0700

    Update

--- CS15-Error/Program.cs
+++ CS15-Error/Program.cs
@@ -4,12 +4,18 @@ namespace CS15_Error
 {
     class Program
     {
+        public static double Thuong(double x, double y) {
+            if (y == 0) {
+                // phát sinh ngoại lệ do số chia bằng 0.
+                Exception myexception = new Exception("Số chia không được bằng 0");
+                throw myexception;
+            }
+            return x / y;
+
+        }
         static void Main(string[] args)
-        {
-
-            int[] mynumbers = new int[] {1,2,3};
-            int i = mynumbers[10];
-
+        { 
+            double z = Thuong(100,0);
         }
     }
 }

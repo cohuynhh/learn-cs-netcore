f466aa318cd3102b497365a6208f875630c3b1f1
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-08-31 13:57:53 +0700

    Update

---
+++ CS19-ObserableCollection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+namespace CS19_ObserableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               //Tạo tập hợp chứa các chuỗi, và bắt sự kiện khi tập hợp thay đổi
+            ObservableCollection<String> obs = new ObservableCollection<String>();
+            obs.CollectionChanged += change;
+
+            //Thay các phần tử tập hợp
+            obs.Add("ZTest1");
+            obs.Add("DTest2");
+            obs.Add("ATest3");
+            obs[2] = "AAAAA";
+            
+            obs.RemoveAt(1);
+            obs.Clear();
+
+        }
+
+        private static void change(object sender, NotifyCollectionChangedEventArgs e) 
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (String s in e.NewItems)
+                        Console.WriteLine($"Thêm :  {s}");
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    Console.WriteLine("Clear");
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (String s in e.OldItems)
+                        Console.WriteLine($"Remove :  {s}");
+                    break;
+                case NotifyCollectionChangedAction.Replace:                
+                    Console.WriteLine("Repaced - " + e.NewItems[0]);
+                break;
+            }
+        }
+    }
+}
---
+++ CS21-ASYNCHRONOUS/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+namespace CS21_ASYNCHRONOUS
+{
+    class Program
+    {
+        static string DownloadWebpage(string url, bool showresult) { 
+            using (var client = new WebClient())
+            {
+                Console.Write("Starting download ...");
+                string content = client.DownloadString(url);
+                Thread.Sleep(3000);
+                if (showresult)
+                    Console.WriteLine(content.Substring(0, 150)); 
+                    
+                return content; 
+            }   
+        } 
+
+
+        public static void WriteLine(string s, ConsoleColor color) {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+        
+    
+        static void Main(string[] args)
+        { 
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId,3} MainThread");
+                     
+
+            Func<object, string> myfunc = (object thamso) => {
+                dynamic ts = thamso;
+                for (int i = 1; i <= 50; i++) 
+                {                    
+                    WriteLine($"{Thread.CurrentThread.ManagedThreadId,3} {ts.x} {i,5} {ts.y}", ConsoleColor.Green);
+                    Thread.Sleep(500);
+                }
+                return "Kết thúc!";
+            };
+            Task<string> task1 = new  Task<string>(myfunc, new {x = "myfunc  ", y = "..."});
+
+
+            Action myaction = () => {
+                for (int i = 1; i <= 50; i++) 
+                {
+                    WriteLine($"{Thread.CurrentThread.ManagedThreadId,3} myaction {i,5}", ConsoleColor.Yellow);
+                    Thread.Sleep(2000);
+                }
+            };
+            Task task2 = new Task(myaction); 
+
+            
+
+            task1.Start();
+            task2.Start(); 
+            
+            
+            Console.ReadKey();
+            Console.ResetColor();
+
+
+            // string url = "https://code.visualstudio.com/";
+            
+            // Func<object,string> downloadtask  = (object thamso) => {
+            //    dynamic prs = thamso; 
+            //    return DownloadWebpage(prs.url, prs.showresult); 
+            // };
+            
+            // Task<string> t1 = new Task<string>(downloadtask, new {url = url, showresult = true}) ;
+            // t1.Start();
+
+            // Console.WriteLine("DO Somthing (2) ...");
+            // Console.ReadLine();
+            // //t1.Wait();
+             
+        }
+    }
+}

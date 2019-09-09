6299ceac935ae0342df4388cfc5476523c66aadb
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-09-09 20:08:53 +0700

    Update

--- CS24-Asp.net-core/HelloWorld/Program.cs
+++ CS24-Asp.net-core/HelloWorld/Program.cs
@@ -15,10 +15,11 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
-}
+} 

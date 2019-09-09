19b05f5c4584e00f3a0eec5d801b807783d9d06b
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-09-10 01:49:07 +0700

    Update

--- CS24-Asp.net-core/HelloWorld/Startup.cs
+++ CS24-Asp.net-core/HelloWorld/Startup.cs
@@ -36,9 +36,36 @@ namespace HelloWorld
                             <head>
                                 <meta charset=""UTF-8"">
                                 <title>Trang web đầu tiên</title>
+                                <link rel=""stylesheet"" href=""/css/bootstrap.min.css"" />
+                                <script src=""/js/jquery.min.js""></script>
+                                <script src=""/js/popper.min.js""></script>
+                                <script src=""/js/bootstrap.min.js""></script>
+
+
                             </head>
                             <body>
-                                <p>Đây là trang web đầu tiên</p>
+                                <nav class=""navbar navbar-expand-lg navbar-dark bg-danger"">
+                                        <a class=""navbar-brand"" href=""#"">Brand-Logo</a>
+                                        <button class=""navbar-toggler"" type=""button"" data-toggle=""collapse"" data-target=""#my-nav-bar"" aria-controls=""my-nav-bar"" aria-expanded=""false"" aria-label=""Toggle navigation"">
+                                                <span class=""navbar-toggler-icon""></span>
+                                        </button>
+                                        <div class=""collapse navbar-collapse"" id=""my-nav-bar"">
+                                        <ul class=""navbar-nav"">
+                                            <li class=""nav-item active"">
+                                                <a class=""nav-link"" href=""#"">Trang chủ</a>
+                                            </li>
+                                        
+                                            <li class=""nav-item"">
+                                                <a class=""nav-link"" href=""#"">Học HTML</a>
+                                            </li>
+                                        
+                                            <li class=""nav-item"">
+                                                <a class=""nav-link disabled"" href=""#"">Gửi bài</a>
+                                            </li>
+                                    </ul>
+                                    </div>
+                                </nav> 
+                                <p class=""display-4 text-danger"">Đây là trang đã có Bootstrap</p>
                             </body>
                             </html>
                 ";

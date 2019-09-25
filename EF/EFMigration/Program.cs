363aa415bf94cedf91114d61d8e1363020647f73
XuanThu <xuanthulab@XuanThus-MacBook-Pro.local>
2019-09-26 03:19:16 +0700

    Update

--- EF/EFMigration/Program.cs
+++ EF/EFMigration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using EFMigration.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,13 +7,14 @@ namespace EFMigration
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using (var webcontext =  new WebContext()) 
             {
                 // webcontext.Database.Migrate();
                 // webcontext.Database.EnsureDeleted();
                 // webcontext.Database.EnsureCreated();
+                await webcontext.Database.MigrateAsync();
             }
         }
     }
